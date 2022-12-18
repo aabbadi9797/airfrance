b626418102c3a4b1319d04e79922db682b2e86ed
aabbadi9797 <a.abbadi9797@gmail.com>
2022-12-18 13:53:31 +0000

    add unit and integration testing

--- src/main/java/com/airfrance/test/model/Country.java
+++ src/main/java/com/airfrance/test/model/Country.java
@@ -1,10 +1,14 @@
 package com.airfrance.test.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
 
 
 /**
@@ -14,6 +18,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @AllArgsConstructor
 @NoArgsConstructor
 @Document(collection = "COUNTRIES")
+@Builder
 public class Country {
     @Id
     private String code;
